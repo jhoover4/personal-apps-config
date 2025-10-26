1e6045d8a9e56ba890920c2055a12e8852da448d
jhoover4 <jordan@hoovermld.com>
2025-10-26 09:52:23 -0500

    Base docker compose

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
-# personal-apps-config
+# Personal Apps Config
 Configuration for Digital Ocean Droplet for hosting personal backend apps.
 
 ## Sites Available
 
 This folder will be symlinked to nginx folder in my droplet.
+
+## Docker Compose
+
+For managing services that 
--- bin/setup-script.sh
+++ bin/setup-script.sh
@@ -3,11 +3,11 @@
 # This is a shell script for installing dependencies for a digital ocean ubuntu vps. The commands here assume you're
 # running ubuntu 20.x
 
-# Setup nginx
+# Setup proxy
 sudo apt update
-sudo apt install nginx
+sudo apt install proxy
 sudo ufw allow 'Nginx HTTP'
 
 # Link server blocks
-ln -s ../sites-available/mineral-catalog /etc/nginx/sites-enabled
-ln -s ../sites-available/pug-or-ugh /etc/nginx/sites-enabled
+ln -s ../sites-available/mineral-catalog /etc/proxy/sites-enabled
+ln -s ../sites-available/pug-or-ugh /etc/proxy/sites-enabled
